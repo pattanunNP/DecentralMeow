60cbde6a4488056344b5e3cd5df61a91373e485a
pattananNP <phattanun19@hotmail.com>
2022-07-09 02:13:09 +0700

    Release: Banking

--- blockchain/contracts/Banking.sol
+++ blockchain/contracts/Banking.sol
@@ -2,15 +2,16 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Banking {
-    uint256 public serialNumber = 0;
-    uint256 public transacNum = 0;
-    uint256 public bankBalance = 0;
+    address private owner;
+
+    uint256 private serialNumber = 0;
+    uint256 private transacNum = 0;
+    uint256 private bankBalance = 0;
 
     struct Account {
         uint256 serial;
         uint256 createAt;
         address creator;
-        string _name;
         uint256 _balance;
         bool doesExist;
     }
@@ -24,20 +25,19 @@ contract Banking {
         uint256 accountSerialNumber;
     }
 
-    mapping(uint256 => Transaction) public transactions;
-    mapping(string => bool) public accountNameExists;
-    mapping(address => Account[]) public accountAddrRecord;
+    mapping(uint256 => Transaction) private transactions;
+    mapping(string => bool) private accountNameExists;
+    mapping(string => Account) private nameAccountRecord;
+    mapping(address => string[]) private accountNameRecord;
 
-    string[] public accountNames;
+    constructor(address owner_address) {
+        owner = owner_address;
+    }
 
-    event AccountCreated(
-        address indexed _creator,
-        uint256 _serialNumber,
-        bytes32 _name,
-        bytes32 _location,
-        uint256 _createdAt,
-        uint256 _balance
-    );
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You're not the smart contract owner!");
+        _;
+    }
 
     event TransactionCompleted(
         address indexed _creator,
@@ -47,23 +47,33 @@ contract Banking {
         bytes32 _transactionType
     );
 
+    function checkExistAccount(string memory _name)
+        public
+        view
+        returns (bool _isExist)
+    {
+        return nameAccountRecord[_name].doesExist;
+    }
+
     function createAccount(string memory _name) public payable {
-        require(!accountNameExists[_name], "Account name already exists");
+        require(
+            !nameAccountRecord[_name].doesExist == true,
+            "Account name already exists"
+        );
 
         address _creator = msg.sender;
-        accountNames.push(_name);
+
         serialNumber++;
         //// Create a new account and store it in the accounts mapping.
-        accountAddrRecord[_creator].push(
-            Account(
-                serialNumber,
-                block.timestamp,
-                _creator,
-                _name,
-                msg.value,
-                true
-            )
+        nameAccountRecord[_name] = Account(
+            serialNumber,
+            block.timestamp,
+            _creator,
+            msg.value,
+            true
         );
+
+        accountNameRecord[_creator].push(_name);
 
         transacNum++; /// increment the transaction number
         // // Create a new transaction new account and store it in the transactions mapping.
@@ -76,17 +86,129 @@ contract Banking {
             serialNumber
         );
         emit TransactionCompleted(
-            _creator,
-            0,
+            msg.sender,
+            msg.value,
             transacNum,
             serialNumber,
             "NewAccount"
-        ); // emit the event for new account creation
+        );
+        // emit the event for new account creation
         accountNameExists[_name] = true;
         bankBalance += msg.value;
     }
 
-    function getUserAccounts() public view returns (Account[] memory) {
-        return accountAddrRecord[msg.sender];
+    function getUserAccounts() public view returns (string[] memory) {
+        return accountNameRecord[msg.sender];
+    }
+
+    function getAccountInfo(string memory _name)
+        public
+        view
+        returns (Account memory)
+    {
+        return nameAccountRecord[_name];
+    }
+
+    function deposit(string memory _name) public payable {
+        // Function for deposit fund to our accounts with _name
+        require(msg.value > 0, "deposit fund can't be zero");
+
+        nameAccountRecord[_name]._balance += msg.value; // add money to user account
+
+        serialNumber++;
+
+        transacNum++; /// increment the transaction number
+
+        emit TransactionCompleted(
+            msg.sender,
+            msg.value,
+            transacNum,
+            serialNumber,
+            "Deposit"
+        );
+
+        bankBalance += msg.value;
+    }
+
+    function withdraw(string memory _name, uint256 amount) public {
+        // Function for withdraw fund to owner wallet
+
+        require(
+            amount > 0 && amount <= nameAccountRecord[_name]._balance,
+            "insufficient funds"
+        );
+
+        payable(msg.sender).transfer(amount);
+
+        nameAccountRecord[_name]._balance -= amount;
+
+        serialNumber++;
+        transacNum++; /// increment the transaction number
+
+        emit TransactionCompleted(
+            msg.sender,
+            amount,
+            transacNum,
+            serialNumber,
+            "Withdraw"
+        );
+        bankBalance -= amount;
+    }
+
+    function transfer(
+        string memory from_name,
+        string memory to_name,
+        uint256 amount
+    ) public {
+        // Function for withdraw fund to owner wallet
+
+        uint256 deduct = (amount * 1) / 100;
+
+        require(
+            amount > 0 && amount <= nameAccountRecord[from_name]._balance,
+            "insufficient funds to transfer"
+        );
+        require(
+            nameAccountRecord[from_name].creator == msg.sender,
+            "Don't have permission"
+        );
+
+        if (
+            nameAccountRecord[from_name].creator ==
+            nameAccountRecord[to_name].creator
+        ) {
+            nameAccountRecord[from_name]._balance -= amount;
+            nameAccountRecord[to_name]._balance += amount;
+        } else {
+            // send to another account deduct 1%
+            nameAccountRecord[from_name]._balance -= amount;
+            nameAccountRecord[to_name]._balance += (amount - deduct);
+        }
+
+        serialNumber++;
+        transacNum++; /// increment the transaction number
+
+        emit TransactionCompleted(
+            msg.sender,
+            amount,
+            transacNum,
+            serialNumber,
+            "transfer success"
+        );
+    }
+
+    function getBankBalance() public view returns (uint256 _balance) {
+        /// Function for get Bank Balance
+        return bankBalance;
+    }
+
+    function getSerialNumber() public view returns (uint256 _serialNumber) {
+        /// Function for get serialNumber
+        return serialNumber;
+    }
+
+    function getTransectionNumber() public view returns (uint256 _transacNum) {
+        /// Function for get transsecNumber
+        return transacNum;
     }
 }
